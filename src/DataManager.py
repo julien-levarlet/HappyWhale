8dafe2a262b6aa81f59c0e5d99384fe88bddc22b
Julien Levarlet <levj2718@usherbrooke.ca>
2022-04-16 12:41:58 -0400

    Bug fix

--- src/DataManager.py
+++ src/DataManager.py
@@ -16,35 +16,44 @@ class DataManager(object):
     """
 
     def __init__(self, annotations_file : str , dataFolderPath : str, batch_size: int = 1,
-                test_percentage : float = 0.2, val_percentage : float = 0.2, **kwargs):
+                test_percentage : float = 0.2, val_percentage : float = 0.2, verbose=False, **kwargs):
         """
-        Input :
-        - annotations_file : CSV file with 2 columns :  * 'image' witch contains image name (imgname.jpg)
+        Args:
+            annotations_file (str): CSV file with 2 columns :  * 'image' witch contains image name (imgname.jpg)
                                                         * 'individual_id' witch contains the id of the animal
-        - dataFolderPath : path to the folder containing all the images
-        - batch_size : batch_size
-        - test_percentage : percentage of images used for test
-        - val_percentage : percentage of images used for validation on all the images that are not used for testing (1- test_percentage)
+            dataFolderPath (str): Path to the folder containing all the images
+            batch_size (int, optional): Batch size. Defaults to 1.
+            test_percentage (float, optional): Percentage of images used for test. Defaults to 0.2.
+            val_percentage (float, optional): Percentage of images used for validation on all the images that are not used for testing (1- test_percentage). Defaults to 0.2.
+            verbose (bool, optional): Display information on datasets. Defaults to False.
         """
         self.batch_size = batch_size
         self.dataFolderPath = dataFolderPath
 
+        # reads data
         df_labels = pd.read_csv(annotations_file)
         # labels need to be categorical 
         df_labels["individual_id"] = df_labels["individual_id"].astype('category').cat.codes
 
-        print(df_labels.head())
-
         # permutation
         df_labels.sample(frac=1)
+        df_labels = df_labels.head(100)
 
+        # separation train/test/validation sets
         val_test_sep = int(df_labels.shape[0] * (1-test_percentage))
         train_val_sep = int(df_labels.shape[0] * (1-test_percentage) * (1-val_percentage) )
 
-        test_data = df_labels[:val_test_sep]
+        test_data = df_labels[val_test_sep:]
         val_data = df_labels[train_val_sep:val_test_sep]
-        train_data = df_labels[train_val_sep:]
+        train_data = df_labels[:train_val_sep]
+        if verbose:
+            print(df_labels.head(5))
+            print("Dataset size :", len(df_labels))
+            print("Size of validation set :", len(val_data))
+            print("Size of test set :", len(test_data))
+            print("Size of train set :", len(train_data))
 
+        # creates Datasets and DataLoaders
         self.train_set = WhaleDataset(train_data, dataFolderPath)
         self.val_set = WhaleDataset(val_data, dataFolderPath)
         self.test_set = WhaleDataset(test_data, dataFolderPath)
--- src/WhaleDataset.py
+++ src/WhaleDataset.py
@@ -12,24 +12,24 @@ class WhaleDataset(data.Dataset):
 
     def __init__(self, dataset : pd.DataFrame,
                  img_dir, transform=None):
-        '''
-        Input :
-        - dataset : dataset, with images name (column 0) and their ids (column 1)
-        - img_dir : path to the folder containing all the images
-        - set : dataset searched, train, validation or test                                                
-        '''
-        self.proba = 0.5
-        self.img_list = dataset
+        """
+        Args:
+            dataset (pd.DataFrame): Dataset, with images name (column 0) and their ids (column 1)
+            img_dir (str): Path to the folder containing all the images
+            transform (Callable, optional): Function used to do the data augmentation. Defaults to None for no augmentation.
+        """
+
+        self.proba = 0.5 # probability of using data augmentation on an image
+        self.img_df = dataset
         self.img_dir = img_dir
-        self.transform_tensor = torchvision.transforms.ToTensor()
         self.transform = transform
 
     def __len__(self):
-        return len(self.img_list)
+        return len(self.img_df)
 
     def __getitem__(self, idx):
-        img = self.img_list.iat[idx,0]
-        label = self.img_list.iat[idx,1]
+        img = self.img_df.iat[idx,0]
+        label = self.img_df.iat[idx,1]
 
         img_path = os.path.join(self.img_dir, img)
         image = cv2.imread(img_path)
@@ -38,7 +38,7 @@ class WhaleDataset(data.Dataset):
 
         if self.transform and self.p < Random.random():
             image = self.transform(image)
-        inter = torchvision.transforms.functional.to_tensor(image)
-        image = inter
+        
+        image = torchvision.transforms.functional.to_tensor(image)
         label = torch.tensor(label)
         return image, label
